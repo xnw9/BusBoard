b720d1d6fba752bb6471c74b138f6eaac9efd52d
Sydney Wu <104062694+xnw9@users.noreply.github.com>
2022-05-06 10:32:58 +0100

    Update departures.js

--- frontend/departures.js
+++ frontend/departures.js
@@ -7,7 +7,7 @@ function result() {
     var inputs = document.getElementById("getcode").elements;
     var postcode = inputs["postcode"].value.replace(" ", "");
 
-    alert("Got postcode: " + postcode)
+    // alert("Got postcode: " + postcode)
 
     // ----- send request and get json result
     // var XMLHttpRequest = require('xhr2');    // remember to remove before using in browser
@@ -17,8 +17,20 @@ function result() {
     xhttp.onload = function () {
         var resText = xhttp.responseText
         let json = JSON.parse(resText);
-        console.log(json)
-        console.log("got json")
+
+        /*// add list elements only when required - failed
+        for (let i = 1; i <= 2; i++) {
+            var theStop = "stop" + String(i)
+
+            for (let j in Object.keys(json[theStop]).length - 2) {  // !
+                var ul = document.getElementById(theStop+"_buses");
+                var li = document.createElement("li");
+                li.appendChild(document.createTextNode(" "));   // !
+                li.setAttribute("id", theStop + "_"+String(j+3)); // added line
+                ul.appendChild(li);
+                console.log(ul, li)
+            }
+        }*/
 
         // change html here - success
         document.getElementById("result").innerHTML = "Result for " + postcode
@@ -27,25 +39,14 @@ function result() {
             let stop = "stop" + String(i)
             document.getElementById(stop).innerHTML = json[stop]["commonName"]
 
+            // empty item lists
+
             for (let j = 1; j <= Object.keys(json[stop]).length - 1; j++) {
-                document.getElementById(stop+"_" + String(i)).innerHTML = format(json[stop][String(i)])
-            }
+                document.getElementById(stop+"_" + String(j)).innerHTML = format(json[stop][String(j)])
+            }   // why used i before and worked?! if used j when did you change it to i?!?
         }
 
-        /*
-        // add list elements only when required - failed
-        for (let i = 1; i <= 2; i++) {
-            var theStop = "stop" + String(i)
-            for (let j in Object.keys(json[theStop]).length - 2) {
-                var ul = document.getElementById(theStop+"_buses");
-                var li = document.createElement("li");
-                li.appendChild(document.createTextNode(" "));
-                li.setAttribute("id", theStop + "_"+String(j+3)); // added line
-                ul.appendChild(li);
-                console.log(ul, li)
-            }
-        }
-                */
+
 
     }
     xhttp.send();
